25d1d674c9e406070e0640deae62083a39747e27
hoogartrupti03-cpu <hoogartrupti03@gmail.com>
2025-11-21 11:09:42 +0530

    Update movie22.py

--- movie22.py
+++ movie22.py
@@ -2,13 +2,17 @@ import sys
 
 # sys.argv[0] is the program name, so actual inputs start from index 1
 if len(sys.argv) != 5:
-    print("Usage: python movie_details.py <movie_name=kantara> <duration=3> <language=kannada> <ticket_price=300>")
-    sys.exit()
-
-movie_name = sys.argv[1]
-duration = sys.argv[2]
-language = sys.argv[3]
-ticket_price = sys.argv[4]
+    movie name = sys.agrv[1]
+    durration = sys.agrv[1]
+    ticket price = sys.agrv[1]
+    language= sys.agrv[1]
+    print ("user input")
+    else
+    print("No given input,using ddefault values")
+    movie_name="kantar"
+    duration="3"
+    language="kannada"
+    ticket_price="300"
 
 print("\n--- Movie Details ---")
 print(f"Movie Name   : {movie_name}")
